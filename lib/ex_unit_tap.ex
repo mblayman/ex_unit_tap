c297b6ec6730dcb276a90753176941d4f4e2a86c
Matt Layman <matthewlayman@gmail.com>
2016-07-24 22:52:00 -0400

    Handle all the events sent to a formatter.

--- lib/ex_unit_tap.ex
+++ lib/ex_unit_tap.ex
@@ -1,2 +1,33 @@
 defmodule ExUnitTap do
+  use GenEvent
+
+  def handle_event({:suite_started, _opts}, config) do
+    IO.puts :suite_started
+    {:ok, config}
+  end
+
+  def handle_event({:suite_finished, _run_us, _load_us}, _config) do
+    IO.puts :suite_finished
+    :remove_handler
+  end
+
+  def handle_event({:case_started, _test_case}, config) do
+    IO.puts :case_started
+    {:ok, config}
+  end
+
+  def handle_event({:case_finished, _test_case}, config) do
+    IO.puts :case_finished
+    {:ok, config}
+  end
+
+  def handle_event({:test_started, _test_case}, config) do
+    IO.puts :test_started
+    {:ok, config}
+  end
+
+  def handle_event({:test_finished, _test_case}, config) do
+    IO.puts :test_finished
+    {:ok, config}
+  end
 end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,2 @@
+ExUnit.configure formatters: [ExUnitTap]
 ExUnit.start()
